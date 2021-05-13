a4776a8b6da9cbd3df7ed491a0bd4cf08a36d7ff
VASILE-IONUȚ-REMUS IGA <vasile.iga@stud.ubbcluj.ro>
2021-05-13 13:23:41 +0300

    Locked screen orientation to portrait.

--- StockMarketSimulator.Android/MainActivity.cs
+++ StockMarketSimulator.Android/MainActivity.cs
@@ -7,7 +7,7 @@ using MediaManager;
 
 namespace StockMarketSimulator.Droid
 {
-    [Activity(Label = "StockMarketSimulator", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+    [Activity(Label = "StockMarketSimulator", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Orientation, ScreenOrientation = ScreenOrientation.Landscape)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
